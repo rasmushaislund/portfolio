3ec72a84e8d6a9eb49a629a0bf2eaff28b7b5d31
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-05-31 12:58:53 +0200

    Create components for CV download and 'contact' linker

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,18 @@
+export function DownloadCV() {
+  return (
+    <button className="appearance-none font-inter font-bold text-text py-2 px-4 bg-green-100 rounded-md hover:bg-green-200">
+      Download CV
+    </button>
+  );
+}
+
+export function GetInTouch() {
+  return (
+    <a
+      href="/#"
+      className="text-text-color-white font-inter font-bold py-2 px-4 border-[3px] border-green-100 rounded-md no-underline hover:bg-gray-800"
+    >
+      Get in touch
+    </a>
+  );
+}
